e258c5c72d861e5fe6fd2268a28039f10d87b327
vbhvbot99 <agarwalvaibhav6024@gmail.com>
2020-05-04 21:31:10 +0530

    modified

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import { Table } from '@jpmorganchase/perspective';
 import { ServerRespond } from './DataStreamer';
-import { DataManipulator } from './DataManipulator';
 import './Graph.css';
 
+/**
+ * Props declaration for <Graph />
+ */
 interface IProps {
   data: ServerRespond[],
 }
 
-interface PerspectiveViewerElement extends HTMLElement {
+/**
+ * Perspective library adds load to HTMLElement prototype.
+ * This interface acts as a wrapper for Typescript compiler.
+ */
+interface PerspectiveViewerElement extends HTMLElement{
   load: (table: Table) => void,
 }
+
+/**
+ * React component that renders Perspective based on data
+ * parsed from its parent through data property.
+ */
 class Graph extends Component<IProps, {}> {
+  // Perspective table
   table: Table | undefined;
 
   render() {
@@ -19,14 +31,17 @@ class Graph extends Component<IProps, {}> {
   }
 
   componentDidMount() {
-    // Get element from the DOM.
+    // Get element to attach the table from the DOM.
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
+      price_abc: 'float',
+      price_def: 'float',
+      ratio: 'float'
       timestamp: 'date',
+      upper_bound: 'float',
+      lower_bound: 'float',
+      trigger_alert: 'float',
     };
 
     if (window.perspective && window.perspective.worker()) {
@@ -34,25 +49,38 @@ class Graph extends Component<IProps, {}> {
     }
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
+
+      // Add more Perspective configurations here.
       elem.load(this.table);
-      elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
-      elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
-      elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
-        timestamp: 'distinct count',
-      }));
-    }
+      elem.setAttribute('view','y_line');
+      elem.setAttribute('row-pivots','["timestamp"]');
+      elem.setAttribute('columns','["ratio","lower_bound","upper_bound","trigger_alert"]');
+      elem.setAttribute('aggregates', JSON.stringify({ 
+      price_abc: 'avg',
+      price_def: 'avg',
+      ratio: 'avg'
+      timestamp: 'distinct count',
+      upper_bound: 'avg',
+      lower_bound: 'avg',
+      trigger_alert: 'avg',
+    }));
   }
 
   componentDidUpdate() {
-    if (this.table) {
-      this.table.update(
-        DataManipulator.generateRow(this.props.data),
-      );
+     if (this.table) {
+         this.table.update([
+             DataManipulator.generateRow(this.props.data),
+            ])
+        }
+    }    
+        // Format the data from ServerRespond to the schema
+        return {
+          stock: el.stock,
+          top_ask_price: el.top_ask && el.top_ask.price || 0,
+          top_bid_price: el.top_bid && el.top_bid.price || 0,
+          timestamp: el.timestamp,
+        };
+      }));
     }
   }
 }
